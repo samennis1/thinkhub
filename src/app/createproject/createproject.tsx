18ed55ba9e34b4eac8e3ebfca2dd106084db2cb7
Brendan Swords <brendanswords21@gmail.com>
2025-03-05 18:28:42 +0000

    added routes for project

---
+++ src/app/createproject/createproject.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Send us a message!</p>
+    </div>
+  );
+}
---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,42 @@
+//make roots for project
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { projects, projectMembers } from "~/server/db/schema";
+
+export const projectRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        description: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(projects).values({
+        name: input.name,
+        description: input.description ?? "",
+        createdBy: ctx.session.user.id,
+      });
+    }),
+
+  addMember: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.number(),
+        userId: z.string(),
+        role: z.enum(["Manager", "Researcher", "Viewer"]),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(projectMembers).values({
+        projectId: input.projectId,
+        userId: input.userId,
+        role: input.role,
+      });
+    }),
+});
