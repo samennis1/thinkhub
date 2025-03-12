bcb5e1f2c25a33cff7b7faf77e46b90a5b2c8c49
Brendan Swords <brendanswords21@gmail.com>
2025-03-12 14:31:29 +0000

    fixed error in thinkhub\src\app\projectdetails\[projectId]\page.tsx

--- src/app/projectdetails/[projectId]/page.tsx
+++ src/app/projectdetails/[projectId]/page.tsx
@@ -222,7 +222,7 @@ const ProjectDetailsPage: React.FC = () => {
   const handleAddMember = async () => {
     setAddMemberError("");
 
-    
+    // Use refetch to retrieve precise user details
     const { data: preciseUser } = await refetchUserId();
 
     if (!preciseUser) {
@@ -233,7 +233,7 @@ const ProjectDetailsPage: React.FC = () => {
     addMemberMutation.mutate(
       {
         projectId: projectId!,
-        email: preciseUser.email, 
+        email: preciseUser.email, // Use `email` instead of `userId`
         role: newMemberRole,
       },
       {
@@ -326,7 +326,7 @@ const ProjectDetailsPage: React.FC = () => {
                   className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-black text-black"
                 >
                   <svg
-                    xmlns="http:
+                    xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
                     className="h-4 w-4"
@@ -367,7 +367,7 @@ const ProjectDetailsPage: React.FC = () => {
                         className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-red-500 text-red-500"
                       >
                         <svg
-                          xmlns="http:
+                          xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
                           fill="currentColor"
                           className="h-4 w-4"
@@ -416,7 +416,7 @@ const ProjectDetailsPage: React.FC = () => {
               className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-black text-black"
             >
               <svg
-                xmlns="http:
+                xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill="currentColor"
                 className="h-4 w-4"
@@ -461,7 +461,7 @@ const ProjectDetailsPage: React.FC = () => {
             className="flex h-12 w-12 items-center justify-center rounded-full border-2 border-black text-black"
           >
             <svg
-              xmlns="http:
+              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
               className="h-8 w-8"
@@ -551,7 +551,7 @@ const ProjectDetailsPage: React.FC = () => {
           style={{
             backgroundColor: "#ffffff",
             backgroundImage:
-              "url(\"data:image/svg+xml,%3Csvg xmlns='http:
+              "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='.5' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cuse fill='%23fcfcfc' href='%23s' y='2'/%3E%3Cuse fill='%23fcfcfc' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='2'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3C/svg%3E\")",
             backgroundAttachment: "fixed",
             backgroundSize: "cover",
           }}
